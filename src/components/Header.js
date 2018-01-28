d3f596a7f079fb3ac8c6dca8710410f04f54de50
Ruslan Nasyrov <ruslan@aimax.pro>
2018-01-28 19:24:03 +0000

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-flash"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Насыров Руслан</h1>
+                <p>Привет! Я Front-end разработчик, пишу код с 2015-ого года. <br /> Занимаюсь версткой сайтов и разработкой интерфейсов на React.</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Works</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contacts</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: React.PropTypes.func,
+    timeout: React.PropTypes.bool
+}
+
+export default Header
